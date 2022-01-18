ae8334ad92f578d267da6c083d765cc17609918a
Kevin <kevinvelasco0299@gmail.com>
2022-01-18 15:36:32 -0500

    Correcciones

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -9,24 +9,24 @@ const pool = new Pool ({
 });
 
 const getPais = async (req, res) => {
-    const response = await pool.query('SELECT * FROM pais');
+    const response = await pool.query('SELECT id,nombre FROM pais');
     res.status(200).json(response.rows);
 };
 const getCiudad = async (req, res) => {
-    const response = await pool.query('SELECT c.id, c.nombre, p.nombre as pais from ciudad c join pais p on c.id_pais=p.id');
+    const response = await pool.query('SELECT c.id, c.nombre,c.poblacion, p.nombre as pais from ciudad c join pais p on c.id_pais=p.id');
     res.status(200).json(response.rows);
 };
 
 
 const getCiudadById = async (req,res) => {
     const id = req.params.id;
-    const response = await pool.query('SELECT c.id, c.nombre, p.nombre as pais from ciudad c join pais p on c.id_pais=p.id WHERE c.id = $1', [id]);    
+    const response = await pool.query('SELECT c.id, c.nombre,c.poblacion, p.nombre as pais,p.id as id_pais from ciudad c join pais p on c.id_pais=p.id WHERE c.id = $1', [id]);    
     res.json(response.rows);
 }
 
 const createCiudad = async (req, res) => {
-    const { nombre, id_pais } = req.body;
-    const response = await pool.query('INSERT INTO ciudad (nombre,id_pais) VALUES ($1, $2)', [nombre,id_pais])
+    const { nombre, id_pais,poblacion } = req.body;
+    const response = await pool.query('INSERT INTO ciudad (nombre,id_pais,poblacion) VALUES ($1, $2,$3)', [nombre,id_pais,poblacion])
     res.json({
         message: 'Ciudad agregada satisfactoriamente',
         body:{
@@ -43,8 +43,8 @@ const deleteCiudad = async (req, res) => {
 
 const updateCiudad = async (req, res) => {
     const id = req.params.id;
-    const { nombre } = req.body;
-    const response = await pool.query('UPDATE ciudad SET nombre = $1 WHERE id = $2', [nombre,id])
+    const { nombre,poblacion,id_pais } = req.body;
+    const response = await pool.query('UPDATE ciudad SET nombre = $1, id_pais = $2, poblacion = $3 WHERE id = $4', [nombre,id_pais,poblacion,id])
     res.json('Ciudad actualizada')
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = Router();
 
 const { getPais, getCiudad, createCiudad,getCiudadById,deleteCiudad,updateCiudad} = require('../controllers/index.controllers')
 router.get('/api/get', getCiudad);
+router.get('/api/getpais', getPais);
 router.get('/api/get/:id', getCiudadById);
 router.post('/api/create', createCiudad);
 router.delete('/api/get/:id', deleteCiudad);
